e4fe5aac1faad435715db9ccca542160633cb2a9
new <new@DESKTOP-GMJ0S4E>
2019-06-09 19:43:38 +0530

    CommandLineRunners Test by using option args and nonoption args

---
+++ src/main/java/com/app/runner/TestRunner.java
@@ -0,0 +1,18 @@
+package com.app.runner;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("From Runner");
+		System.out.println(args);
+		System.out.println(Arrays.asList(args));
+	}
+
+}
